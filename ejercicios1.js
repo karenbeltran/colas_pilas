a6966b52ccaa49526fd336f68b438b1eedb83c32
karenbeltran <beltranramirezkaren@gmail.com>
2017-03-14 15:12:00 +0000

    colas_pilas

---
+++ ejercicios1.js
@@ -0,0 +1,12 @@
+var nombre = [];
+
+var nombres = ["karen", "jacqueline" , "arturo" , "elizabeth" , "kepler"];
+var apellidos = ["beltran" , "ramirez" , "valdez" , "juarez" , "zavala" ];
+//for (i = nombres.length-1;i >= 0; i--){
+	// nombres.push(nombres[1] + " " + apellidos[1]);
+// }
+ nombres.forEach(function NombreCompleto(nombres, i){
+ 	nombre.push(nombres + " " + apellido[i])
+ 
+ });
+ 	console.log (nombre);
